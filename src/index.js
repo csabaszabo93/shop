2fbe8541b9478d5de207a60bc195766b58e39b3c
Csaba Szabó <szabcsabest@gmail.com>
2019-08-12 10:23:39 +0200

    store provided to app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,17 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import rootReducer from "./components/reducers/RootReducer";
+import {createStore} from "redux";
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    document.getElementById('root')
+);
 
 serviceWorker.unregister();
