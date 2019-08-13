eccaa3dda6554825fea0248735befd97e21418cf
Csaba Szabó <szabcsabest@gmail.com>
2019-08-13 17:42:23 +0200

    styled menu

--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import MenuTile from '../menu/MenuTile';
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBackward} from "@fortawesome/free-solid-svg-icons";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
 
 const Menu = ({visible, menuTitles, selectedMenuItem, selectionHandler, closeMenu}) => {
   if(visible) {
     return (
         <div id="modal">
           <div id="menu">
-            Menu
+            <div onClick={closeMenu}>
+              <FontAwesomeIcon icon={faBars}/>
+            </div>
             {menuTitles.map((menuTitle, index) => (
               <MenuTile key={index} title={menuTitle} selectionHandler={selectionHandler} highlight={menuTitle === selectedMenuItem}/>
             ))}
-            <div onClick={closeMenu}>
-              <FontAwesomeIcon icon={faBackward}/>
-            </div>
           </div>
         </div>
     )
